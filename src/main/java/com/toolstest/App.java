fa7ec8933ea8c73fda94098373b75ea3369b78a0
mansi910 <mansi09101993@gmail.com>
2016-07-21 10:18:05 +0530

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -6,12 +6,16 @@ package com.toolstest;
 public class App {
 
   
+  
   private String name;
   
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
+    public static string test(){  
       int num;
-        
+      
+      try{  
         System.out.println("Hello World!");
-       
+      }
+      
     }
 }
