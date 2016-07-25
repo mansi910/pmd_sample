9348c00f136df0a4214940bd07e357b01381b45c
mansi910 <mansi09101993@gmail.com>
2016-07-25 11:39:43 +0530

    Update App.java

--- src/main/java/com/toolstest/App.java
+++ src/main/java/com/toolstest/App.java
@@ -14,6 +14,8 @@ public class App {
     public static string test(){  
       int num;
       
+      public class Bar { public String foo() { try { throw new Exception( "My Exception" ); } catch (Exception e) { throw e; } finally { return "A. O. K."; // return not recommended here } } }
+      
       try{  
         System.out.println("Hello World!");
         if(num > 0){
